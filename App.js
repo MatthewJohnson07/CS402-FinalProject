03618e6396cf2ff5784835be79fc087db66a6abf
MatthewJohnson07 <matthewjohnson101@u.boisestate.edu>
2022-04-24 14:49:08 -0600

    Imported Libraries

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GameEngine } from 'react-native-game-engine';
 
 export default function App() {
   return (
