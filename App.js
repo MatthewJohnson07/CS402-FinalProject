9708140d376a6761753a27d95ce3e718d4a68ed7
lukaskarlsson290 <73920347+lukaskarlsson290@users.noreply.github.com>
2022-04-24 19:10:13 -0600

    Added game board functionality

--- App.js
+++ App.js
@@ -1,21 +1,121 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef, useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { GameEngine } from "react-native-game-engine";
+import Constants from "./systems/Constants";
+import Head from "./components/Head";
+import Key from "./components/Key";
+import Door from "./components/Door";
+import GameLoops from "./systems/GameLoops";
+ 
 
 export default function App() {
+  const BoardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
+  const engine = useRef(null);
+  const [isGameRunning, setIsGameRunning] = useState(true);
+
+  const randomPositions = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.canvas}>
+    <GameEngine
+        ref={engine}
+        style={{
+          width: BoardSize,
+          height: BoardSize,
+          flex: null,
+          backgroundColor: "white",
+        }}
+        entities={{
+          head: {
+            position: [0,0],
+            size: Constants.CELL_SIZE,
+            nextMove: 1,
+            xPos: 0,
+            yPos: 0,
+            keyGrabbed: false,
+            renderer: <Head />
+          }, 
+          key: {
+             position: [
+              randomPositions(0, Constants.GRID_SIZE - 1),
+              randomPositions(0, Constants.GRID_SIZE - 1),
+            ],
+          
+            size: Constants.CELL_SIZE,
+            renderer: <Key />,
+          },
+
+          door: {
+            position: [
+              Constants.GRID_SIZE -1, 0
+            ], 
+            size: Constants.CELL_SIZE,
+            renderer: <Door />,
+          }
+        }}
+        systems={[GameLoops]}
+        running={isGameRunning}
+        onEvent={(e) => {
+          switch (e) {
+            case "game-over":
+              alert("Game over!");
+              setIsGameRunning(false);
+              return;
+          }
+        }}
+        />
+        <View style={styles.controlContainer}>
+        <View style={styles.controllerRow}>
+          <TouchableOpacity onPress={() => engine.current.dispatch("move-up")}>
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.controllerRow}>
+          <TouchableOpacity
+            onPress={() => engine.current.dispatch("move-left")}
+          >
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
+          <View style={[styles.controlBtn, { backgroundColor: null }]} />
+          <TouchableOpacity
+            onPress={() => engine.current.dispatch("move-right")}
+          >
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.controllerRow}>
+          <TouchableOpacity
+            onPress={() => engine.current.dispatch("move-down")}
+          >
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  canvas: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  controlContainer: {
+    marginTop: 10,
+  },
+  controllerRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  controlBtn: {
+    backgroundColor: "yellow",
+    width: 100,
+    height: 100,
   },
 });
