4ca0a3952578b5a5556fc1a3be64e639d94d35e0
rossrippee <74170962+rossrippee@users.noreply.github.com>
2022-04-25 14:09:28 -0600

    Blue buttons

--- App.js
+++ App.js
@@ -114,7 +114,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   controlBtn: {
-    backgroundColor: "yellow",
+    backgroundColor: "blue",
     width: 100,
     height: 100,
   },
