27eeb5159e85cea3e148e44767c255a875c4f4a5
lukaskarlsson290 <73920347+lukaskarlsson290@users.noreply.github.com>
2022-04-25 14:01:17 -0600

    Fixed typo in background color

--- components/Key.js
+++ components/Key.js
@@ -6,7 +6,7 @@ export default function Key({ position, size }) {
       style={{
         width: size,
         height: size,
-        backgroundColor: "ywllow",
+        backgroundColor: "yellow",
         position: "absolute",
         left: position[0] * size,
         top: position[1] * size,
