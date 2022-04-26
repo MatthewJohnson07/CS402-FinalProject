493139659d2161774ba80ebfc03ca77eea39d2a9
lukaskarlsson290 <73920347+lukaskarlsson290@users.noreply.github.com>
2022-04-26 11:36:11 -0600

    Fixed some formatting issues on mobile

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useRef, useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Button } from 'react-native';
+import React, {useRef, useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Button, SafeAreaView} from 'react-native';
 import { GameEngine } from "react-native-game-engine";
 import Constants from "./systems/Constants";
 import Head from "./components/Head";
 import Key from "./components/Key";
 import Door from "./components/Door";
 import GameLoops from "./systems/GameLoops";
-
+ 
 const BoardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
 
 export default function App() {
-  //const BoardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
+  
   const engine = useRef(null);
   const [isGameRunning, setIsGameRunning] = useState(true);
   const [viewmode, setVMode] = useState(false);
@@ -19,92 +19,92 @@ export default function App() {
   const randomPositions = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
+  
+var screenChoice = ""
+ 
+ if(viewmode)
+ {
+  screenChoice =   <SafeAreaView style={styles.canvas}>
+    <GameEngine
+        ref={engine}
+        style={{
+          width: BoardSize,
+          height: BoardSize,
+          flex: null,
+          backgroundColor: "white",
+        }}
+        entities={{
+          head: {
+            position: [0,0],
+            size: Constants.CELL_SIZE,
+            nextMove: 1,
+            xPos: 0,
+            yPos: 0,
+            keyGrabbed: false,
+            renderer: <Head />
+          }, 
+          key: {
+             position: [
+              randomPositions(0, Constants.GRID_SIZE - 1),
+              randomPositions(0, Constants.GRID_SIZE - 1),
+            ],
+          
+            size: Constants.CELL_SIZE,
+            renderer: <Key />,
+          },
 
-  var screenChoice = ""
-
-  if (viewmode) {
-    screenChoice =
-      <View style={styles.canvas}>
-        <GameEngine
-          ref={engine}
-          style={{
-            width: BoardSize,
-            height: BoardSize,
-            flex: null,
-            backgroundColor: "white",
-          }}
-          entities={{
-            head: {
-              position: [0, 0],
-              size: Constants.CELL_SIZE,
-              nextMove: 1,
-              xPos: 0,
-              yPos: 0,
-              keyGrabbed: false,
-              renderer: <Head />
-            },
-            key: {
-              position: [
-                randomPositions(0, Constants.GRID_SIZE - 1),
-                randomPositions(0, Constants.GRID_SIZE - 1),
-              ],
-
-              size: Constants.CELL_SIZE,
-              renderer: <Key />,
-            },
-
-            door: {
-              position: [
-                Constants.GRID_SIZE - 1, 0
-              ],
-              size: Constants.CELL_SIZE,
-              renderer: <Door />,
-            }
-          }}
-          systems={[GameLoops]}
-          running={isGameRunning}
-          onEvent={(e) => {
-            switch (e) {
-              case "game-over":
-                alert("Game over!");
-                setIsGameRunning(false);
-                return;
-            }
-          }}
+          door: {
+            position: [
+              Constants.GRID_SIZE -1, 0
+            ], 
+            size: Constants.CELL_SIZE,
+            renderer: <Door />,
+          }
+        }}
+        systems={[GameLoops]}
+        running={isGameRunning}
+        onEvent={(e) => {
+          switch (e) {
+            case "game-over":
+              alert("Game over!");
+              setIsGameRunning(false);
+              return;
+          }
+        }}
         />
         <View style={styles.controlContainer}>
-          <View style={styles.controllerRow}>
-            <TouchableOpacity onPress={() => engine.current.dispatch("move-up")}>
-              <View style={styles.controlBtn} />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.controllerRow}>
-            <TouchableOpacity
-              onPress={() => engine.current.dispatch("move-left")}
-            >
-              <View style={styles.controlBtn} />
-            </TouchableOpacity>
-            <View style={[styles.controlBtn, { backgroundColor: null }]} />
-            <TouchableOpacity
-              onPress={() => engine.current.dispatch("move-right")}
-            >
-              <View style={styles.controlBtn} />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.controllerRow}>
-            <TouchableOpacity
-              onPress={() => engine.current.dispatch("move-down")}
-            >
-              <View style={styles.controlBtn} />
-            </TouchableOpacity>
-          </View>
+        <View style={styles.controllerRow}>
+          <TouchableOpacity onPress={() => engine.current.dispatch("move-up")}>
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.controllerRow}>
+          <TouchableOpacity
+            onPress={() => engine.current.dispatch("move-left")}
+          >
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
+          <View style={[styles.controlBtn, { backgroundColor: null }]} />
+          <TouchableOpacity
+            onPress={() => engine.current.dispatch("move-right")}
+          >
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.controllerRow}>
+          <TouchableOpacity
+            onPress={() => engine.current.dispatch("move-down")}
+          >
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
         </View>
       </View>
-  }
-  else {
-
-    screenChoice =
-      <View style={styles.scoreScreenContainer}>
+    </SafeAreaView>
+ }
+ else {
+  
+screenChoice = 
+<SafeAreaView style={styles.canvas}>
         <View style={styles.backgroundImage}>
           <Text style={styles.gameNameText}>
             GENERIC TOP-DOWN RPG STYLE GAME
@@ -121,53 +121,54 @@ export default function App() {
             />
           </View>
         </View>
-
         <View style={styles.controlContainer}>
           <View style={styles.controllerRow}>
-            <TouchableOpacity onPress={() => engine.current.dispatch("move-up")}>
+            <TouchableOpacity>
               <View style={styles.controlBtn} />
             </TouchableOpacity>
           </View>
           <View style={styles.controllerRow}>
             <TouchableOpacity
-              onPress={() => engine.current.dispatch("move-left")}
+             
             >
               <View style={styles.controlBtn} />
             </TouchableOpacity>
             <View style={[styles.controlBtn, { backgroundColor: null }]} />
             <TouchableOpacity
-              onPress={() => engine.current.dispatch("move-right")}
+             
             >
               <View style={styles.controlBtn} />
             </TouchableOpacity>
           </View>
           <View style={styles.controllerRow}>
             <TouchableOpacity
-              onPress={() => engine.current.dispatch("move-down")}
+             
             >
               <View style={styles.controlBtn} />
             </TouchableOpacity>
           </View>
         </View>
-      </View>
+        </SafeAreaView>
 
-  }
+  
+ }
 
-  return (screenChoice);
-
-
-
+return(screenChoice);
 
 
-  function switchMode() {
-    if (viewmode) // in grid mode
-    {
-      setVMode(false);
-    }
-    else // in Preview mode
-    {
-      setVMode(true);
-    }
+
+
+
+ function switchMode()
+  {
+     if (viewmode) // in grid mode
+     {
+         setVMode(false);
+     }
+     else // in Preview mode
+     {
+          setVMode(true);
+     }
   }
 }
 
@@ -232,4 +233,5 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
   }
+   
 });
