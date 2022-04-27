55dbc56dc58c35cd4634a8d917b24b3cdacd0a3e
rossrippee <74170962+rossrippee@users.noreply.github.com>
2022-04-26 19:05:13 -0600

    Functional Menu Screen

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useRef, useState, useEffect} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Button, SafeAreaView, VirtualizedList} from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Button, SafeAreaView, VirtualizedList } from 'react-native';
 import { GameEngine } from "react-native-game-engine";
 import Constants from "./systems/Constants";
 import Head from "./components/Head";
 import Key from "./components/Key";
 import Door from "./components/Door";
 import GameLoops from "./systems/GameLoops";
-import {Item} from './components/ListItem.js'
-import {loadList,saveList} from './components/RemoteAccess.js'
+import { Item } from './components/ListItem.js'
+import { loadList, saveList } from './components/RemoteAccess.js'
 import { Stopwatch } from 'react-native-stopwatch-timer';
- 
+
 const BoardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
 
 // declare our initial list of Items to show in the Virtual List.
 var startlist = [
-  {key: "RRR"}
+  { key: "RRR" }
 ];
 
 export default function App() {
 
 
-var emptydata = [];
-    //necessary functions for the <VirtualList> Component
+  var emptydata = [];
+  //necessary functions for the <VirtualList> Component
   const getItemCount = (data) => list.length; // return the total number of items in the actual list.  Ignore data
   const getItem = (data, index) => list[index]; // get an individual item from the actual data list. Ignore data
 
@@ -32,23 +32,24 @@ var emptydata = [];
   //const response = loadList(urladdress,startlist)
   const [list, setlist] = useState(startlist);
   const [secondlist, setsecondlist] = useState(startlist);
-  const [autosave,setsave] = useState(false);
+  const [autosave, setsave] = useState(false);
   const [isStopwatchStart, setIsStopwatchStart] = useState(false);
   const [resetStopwatch, setResetStopwatch] = useState(false);
   var screenChoice = ""
-  
+
   const engine = useRef(null);
   const [isGameRunning, setIsGameRunning] = useState(true);
   const [viewmode, setVMode] = useState(false);
   const [viewLeaderboard, setLBoard] = useState(false);
+  const [menuOptionOne, setMenuOption] = useState(true);
 
   const randomPositions = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
-  
- 
 
-    
+
+
+
   // the following functions modify the data in the list.
   // read data from remote URL
   function loadButton() {
@@ -68,7 +69,7 @@ var emptydata = [];
     );
   };
 
-   var avirtlist = (
+  var avirtlist = (
     <VirtualizedList
       styles={styles.list}
       data={emptydata}
@@ -79,216 +80,280 @@ var emptydata = [];
       getItem={getItem}
     />
   );
- 
-if(viewLeaderboard){
-    screenChoice = 
-    <SafeAreaView>
-    
-             <View style={styles.startButton}>
-            <Button onPress={() => goToLeaderboard()}
-              title="Go Back"
-            />
-            {avirtlist}
-    </View>
-    </SafeAreaView>
-   
-  }
-else
- if(viewmode)
- {
-  
-  screenChoice =   <SafeAreaView style={styles.canvas}>
-    <GameEngine
-        ref={engine}
-        style={{
-          width: BoardSize,
-          height: BoardSize,
-          flex: null,
-          backgroundColor: "white",
-        }}
-        entities={{
-          head: {
-            position: [0,0],
-            size: Constants.CELL_SIZE,
-            nextMove: 1,
-            xPos: 0,
-            yPos: 0,
-            keyGrabbed: false,
-            renderer: <Head />
-          }, 
-          key: {
-             position: [
-              randomPositions(0, Constants.GRID_SIZE - 1),
-              randomPositions(0, Constants.GRID_SIZE - 1),
-            ],
-          
-            size: Constants.CELL_SIZE,
-            renderer: <Key />,
-          },
 
-          door: {
-            position: [
-              Constants.GRID_SIZE -1, 0
-            ], 
-            size: Constants.CELL_SIZE,
-            renderer: <Door />,
-          }
-        }}
-        systems={[GameLoops]}
-        running={isGameRunning}
-        onEvent={(e) => {
-          switch (e) {
-            case "game-over":
-              alert("Game over!");
-              setIsGameRunning(false);
-              setVMode(false);
-              return;
-          }
-        }}
+  if (viewLeaderboard) {
+    screenChoice =
+      <SafeAreaView>
+
+        <View style={styles.startButton}>
+          <Button onPress={() => goToLeaderboard()}
+            title="Go Back"
+          />
+          {avirtlist}
+        </View>
+      </SafeAreaView>
+
+  }
+  else
+    if (viewmode) {
+
+      screenChoice = <SafeAreaView style={styles.canvas}>
+        <GameEngine
+          ref={engine}
+          style={{
+            width: BoardSize,
+            height: BoardSize,
+            flex: null,
+            backgroundColor: "white",
+          }}
+          entities={{
+            head: {
+              position: [0, 0],
+              size: Constants.CELL_SIZE,
+              nextMove: 1,
+              xPos: 0,
+              yPos: 0,
+              keyGrabbed: false,
+              renderer: <Head />
+            },
+            key: {
+              position: [
+                randomPositions(0, Constants.GRID_SIZE - 1),
+                randomPositions(0, Constants.GRID_SIZE - 1),
+              ],
+
+              size: Constants.CELL_SIZE,
+              renderer: <Key />,
+            },
+
+            door: {
+              position: [
+                Constants.GRID_SIZE - 1, 0
+              ],
+              size: Constants.CELL_SIZE,
+              renderer: <Door />,
+            }
+          }}
+          systems={[GameLoops]}
+          running={isGameRunning}
+          onEvent={(e) => {
+            switch (e) {
+              case "game-over":
+                alert("Game over!");
+                setIsGameRunning(false);
+                setVMode(false);
+                return;
+            }
+          }}
         />
         <View style={styles.controlContainer}>
-        <View style={styles.controllerRow}>
-          <TouchableOpacity onPress={() => engine.current.dispatch("move-up")}>
-            <View style={styles.controlBtn} />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.controllerRow}>
-          <TouchableOpacity
-            onPress={() => engine.current.dispatch("move-left")}
-          >
-            <View style={styles.controlBtn} />
-          </TouchableOpacity>
-          <View style={[styles.controlBtn, { backgroundColor: null }]} />
-          <TouchableOpacity
-            onPress={() => engine.current.dispatch("move-right")}
-          >
-            <View style={styles.controlBtn} />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.controllerRow}>
-          <TouchableOpacity
-            onPress={() => engine.current.dispatch("move-down")}
-          >
-            <View style={styles.controlBtn} />
-          </TouchableOpacity>
-        </View>
-      </View>
-	  
-	  <View style={styles.alterContainer}>
-			<TouchableOpacity
-				
-            >
-              <View style={styles.aBtn} />
-            </TouchableOpacity>
-      </View>
-	  <Stopwatch
-            laps
-            msecs
-            start={isStopwatchStart}
-            //To start
-            reset={resetStopwatch}
-            //To reset
-            getTime={(time) => {
-              
-            }}
-          />
-	  
-    </SafeAreaView>
- }
- else {
-  
-screenChoice = 
-<SafeAreaView style={styles.canvas}>
-        <View style={styles.backgroundImage}>
-          <Text style={styles.gameNameText}>
-            GENERIC TOP-DOWN RPG STYLE GAME
-          </Text>
-          <Text style={styles.mainAuthorTextWinkWinkItsJustAJokeYouKnow}>
-            BY ROSS RIPPEE
-          </Text>
-          <Text style={styles.friendshipIsMagic}>
-            and Matthew, Lukas and Javi
-          </Text>
-          <View style={styles.startButton}>
-            <Button onPress={() => switchMode()}
-              title="Start Game"
-            />
-            <Button  onPress={() => goToLeaderboard()} 
-            title="Leaderboard" />
-          </View>
-        </View>
-        <View style={styles.controlContainer}>
           <View style={styles.controllerRow}>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => engine.current.dispatch("move-up")}>
               <View style={styles.controlBtn} />
             </TouchableOpacity>
           </View>
           <View style={styles.controllerRow}>
             <TouchableOpacity
-             
+              onPress={() => engine.current.dispatch("move-left")}
             >
               <View style={styles.controlBtn} />
             </TouchableOpacity>
             <View style={[styles.controlBtn, { backgroundColor: null }]} />
             <TouchableOpacity
-             
+              onPress={() => engine.current.dispatch("move-right")}
             >
               <View style={styles.controlBtn} />
             </TouchableOpacity>
           </View>
           <View style={styles.controllerRow}>
             <TouchableOpacity
-             
+              onPress={() => engine.current.dispatch("move-down")}
             >
               <View style={styles.controlBtn} />
             </TouchableOpacity>
           </View>
         </View>
-		
-		<View style={styles.alterContainer}>
-			<TouchableOpacity
-				onPress={() => engine.current.dispatch("a")}
-            >
-              <View style={styles.aBtn} />
-            </TouchableOpacity>
-		</View>
-		
-</SafeAreaView>
- 
- }
 
-return(screenChoice);
+        <View style={styles.alterContainer}>
+          <TouchableOpacity
+
+          >
+            <View style={styles.aBtn} />
+          </TouchableOpacity>
+        </View>
+        <Stopwatch
+          laps
+          msecs
+          start={isStopwatchStart}
+          //To start
+          reset={resetStopwatch}
+          //To reset
+          getTime={(time) => {
+
+          }}
+        />
+
+      </SafeAreaView>
+    }
+    else {
+
+      if (menuOptionOne) {
+        screenChoice =
+          <SafeAreaView style={styles.canvas}>
+            <View style={styles.backgroundImage}>
+              <Text style={styles.gameNameText}>
+                GENERIC TOP-DOWN RPG STYLE GAME
+              </Text>
+              <Text style={styles.mainAuthorJoke}>
+                BY ROSS RIPPEE
+              </Text>
+              <Text style={styles.friendshipIsMagic}>
+                and Matthew, Lukas and Javi
+              </Text>
+              <View style={styles.startButtonView}>
+                <Text style={styles.selectedText}>
+                  START GAME
+                </Text>
+              </View>
+              <View style={styles.leaderboardButtonView}>
+                <Text style={styles.nonselectedText}>
+                  LEADERBOARD
+                </Text>
+              </View>
+            </View>
+            <View style={styles.controlContainer}>
+              <View style={styles.controllerRow}>
+                <TouchableOpacity>
+                  <View style={styles.controlBtn} />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.controllerRow}>
+                <TouchableOpacity
+
+                >
+                  <View style={styles.controlBtn} />
+                </TouchableOpacity>
+                <View style={[styles.controlBtn, { backgroundColor: null }]} />
+                <TouchableOpacity
+
+                >
+                  <View style={styles.controlBtn} />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.controllerRow}>
+                <TouchableOpacity
+                  onPress={() => setMenuOption(false)}
+                >
+                  <View style={styles.controlBtn} />
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <View style={styles.alterContainer}>
+              <TouchableOpacity
+                onPress={() => switchMode()}
+              >
+                <View style={styles.aBtn} />
+              </TouchableOpacity>
+            </View>
+
+          </SafeAreaView >
+      }
+      else {
+        screenChoice =
+          <SafeAreaView style={styles.canvas}>
+            <View style={styles.backgroundImage}>
+              <Text style={styles.gameNameText}>
+                GENERIC TOP-DOWN RPG STYLE GAME
+              </Text>
+              <Text style={styles.mainAuthorJoke}>
+                BY ROSS RIPPEE
+              </Text>
+              <Text style={styles.friendshipIsMagic}>
+                and Matthew, Lukas and Javi
+              </Text>
+              <View style={styles.startButtonView}>
+                <Text style={styles.nonselectedText}>
+                  START GAME
+                </Text>
+              </View>
+              <View style={styles.leaderboardButtonView}>
+                <Text style={styles.selectedText}>
+                  LEADERBOARD
+                </Text>
+              </View>
+            </View>
+            <View style={styles.controlContainer}>
+              <View style={styles.controllerRow}>
+                <TouchableOpacity onPress={() => setMenuOption(true)}>
+                  <View style={styles.controlBtn} />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.controllerRow}>
+                <TouchableOpacity
+
+                >
+                  <View style={styles.controlBtn} />
+                </TouchableOpacity>
+                <View style={[styles.controlBtn, { backgroundColor: null }]} />
+                <TouchableOpacity
+
+                >
+                  <View style={styles.controlBtn} />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.controllerRow}>
+                <TouchableOpacity
+
+                >
+                  <View style={styles.controlBtn} />
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <View style={styles.alterContainer}>
+              <TouchableOpacity
+                onPress={() => goToLeaderboard()}
+              >
+                <View style={styles.aBtn} />
+              </TouchableOpacity>
+            </View>
+
+          </SafeAreaView >
+      }
+
+
+
+    }
+
+  return (screenChoice);
 
 
 
 
 
- function switchMode()
-  {
-     if (viewmode) // in grid mode
-     {
-         setVMode(false);
-         setIsStopwatchStart(false);
-         setResetStopwatch(true);
-     }
-     else // in Preview mode
-     {
-          setVMode(true);
-          setIsStopwatchStart(true);
-          setIsGameRunning(true);
-     }
+  function switchMode() {
+    if (viewmode) // in grid mode
+    {
+      setVMode(false);
+      setIsStopwatchStart(false);
+      setResetStopwatch(true);
+    }
+    else // in Preview mode
+    {
+      setVMode(true);
+      setIsStopwatchStart(true);
+      setIsGameRunning(true);
+    }
   }
 
-function goToLeaderboard()
-{
-    if(viewLeaderboard)
-    {
+  function goToLeaderboard() {
+    if (viewLeaderboard) {
       setLBoard(false);
-    } else
-    {
+    } else {
       setLBoard(true);
     }
-}
+  }
 }
 
 
@@ -305,12 +370,12 @@ const styles = StyleSheet.create({
   },
   controlContainer: {
     marginTop: 20,
-	marginLeft: -170,
+    marginLeft: -170,
     flex: 0.8,
   },
   alterContainer: {
     marginTop: 10,
-	marginLeft: 290,
+    marginLeft: 290,
     flex: 6.6,
   },
   controllerRow: {
@@ -324,10 +389,10 @@ const styles = StyleSheet.create({
     height: 90,
   },
   aBtn: {
-	backgroundColor: "green",
-	width: 90,
-	height: 90,
-	borderRadius: 50,
+    backgroundColor: "green",
+    width: 90,
+    height: 90,
+    borderRadius: 50,
   },
   startButton: {
     width: '100%',
@@ -344,6 +409,8 @@ const styles = StyleSheet.create({
     width: BoardSize,
     height: BoardSize,
     backgroundColor: 'black',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   gameNameText: {
     marginTop: 10,
@@ -353,7 +420,7 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
   },
-  mainAuthorTextWinkWinkItsJustAJokeYouKnow: {
+  mainAuthorJoke: {
     fontFamily: 'Georgia',
     fontWeight: 'italics',
     fontSize: '24px',
@@ -365,6 +432,23 @@ const styles = StyleSheet.create({
     fontSize: '16px',
     color: 'white',
     textAlign: 'center',
-  }
-   
+  },
+  startButtonView: {
+    position: 'absolute',
+    top: (BoardSize * 3) / 5,
+  },
+  selectedText: {
+    color: 'black',
+    textAlign: 'center',
+    backgroundColor: 'yellow',
+    width: '100%',
+  },
+  leaderboardButtonView: {
+    position: 'absolute',
+    top: (BoardSize * 4) / 5,
+  },
+  nonselectedText: {
+    color: 'white',
+    textAlign: 'center',
+  },
 });
