00ffb3e2102b4800eaad416d2b65e8dd7b6052ca
rossrippee <74170962+rossrippee@users.noreply.github.com>
2022-05-03 09:00:34 -0600

    Made a common file for playing sfx in any view

---
+++ PlaySounds.js
@@ -0,0 +1,66 @@
+import { Audio } from 'expo-av';
+
+async function loadedGame() {
+    const { sound } = await Audio.Sound.createAsync(
+        require('./sounds/KH1LoadGame.mp3')
+    );
+    await sound.playAsync();
+}
+
+async function movedCursor() {
+    const { sound } = await Audio.Sound.createAsync(
+        require('./sounds/KH1CursorMoveOriginal.mp3')
+    );
+    await sound.playAsync();
+}
+
+async function pressedPurple() {
+    const { sound } = await Audio.Sound.createAsync(
+        require('./sounds/KH1Select.mp3')
+    );
+    await sound.playAsync();
+}
+
+async function lockedDoor() {
+    const { sound } = await Audio.Sound.createAsync(
+        require('./sounds/SH1LockedDoor.mp3')
+    );
+    await sound.playAsync();
+}
+
+async function openedDoor() {
+    const { sound } = await Audio.Sound.createAsync(
+        require('./sounds/SH1OpenedDoor.mp3')
+    );
+    await sound.playAsync();
+}
+
+async function doorUnlocked() {
+    const { sound } = await Audio.Sound.createAsync(
+        require('./sounds/SH1DoorUnlocked.mp3')
+    );
+    await sound.playAsync();
+}
+
+export default function PlaySound(songToPlay) {
+    switch (songToPlay) {
+        case 'loadedGame':
+            loadedGame();
+            break;
+        case 'movedCursor':
+            movedCursor();
+            break;
+        case 'pressedPurple':
+            pressedPurple();
+            break;
+        case 'lockedDoor':
+            lockedDoor();
+            break;
+        case 'openedDoor':
+            openedDoor();
+            break;
+        case 'doorUnlocked':
+            doorUnlocked();
+            break;
+    }
+}
