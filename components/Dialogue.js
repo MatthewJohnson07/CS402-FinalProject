59959b1134eb5b01754f207227d8e685cbcac65e
rossrippee <74170962+rossrippee@users.noreply.github.com>
2022-05-03 16:39:40 -0600

    Added more NPC dialogue options

--- components/Dialogue.js
+++ components/Dialogue.js
@@ -5,6 +5,12 @@ const message2 = require('../assets/keyDialogue2.png');
 const message3 = require('../assets/keyDialogue3.png');
 const message4 = require('../assets/keyDialogue4.png');
 const gotKey = require('../assets/keyAcquired.png');
+const extraDialogue1 = require('../assets/extraDialogue1.png');
+const extraDialogue2 = require('../assets/extraDialogue2.png');
+const extraDialogue3 = require('../assets/extraDialogue3.png');
+const unlockedDialogue1 = require('../assets/unlockedDialogue1.png')
+const unlockedDialogue2 = require('../assets/unlockedDialogue2.png')
+const unlockedDialogue3 = require('../assets/unlockedDialogue3.png')
 export default function Dialogue({ position, size, dialogueNumber, location, display }) {
     if (display) {
         if (dialogueNumber == 1) {
@@ -22,7 +28,7 @@ export default function Dialogue({ position, size, dialogueNumber, location, dis
                 />
             );
         }
-        if (dialogueNumber == 2) {
+        else if (dialogueNumber == 2) {
             return (
                 <Image
                     style={{
@@ -37,7 +43,7 @@ export default function Dialogue({ position, size, dialogueNumber, location, dis
                 />
             );
         }
-        if (dialogueNumber == 3) {
+        else if (dialogueNumber == 3) {
             return (
                 <Image
                     style={{
@@ -52,7 +58,7 @@ export default function Dialogue({ position, size, dialogueNumber, location, dis
                 />
             );
         }
-        if (dialogueNumber == 4) {
+        else if (dialogueNumber == 4) {
             return (
                 <Image
                     style={{
@@ -67,7 +73,7 @@ export default function Dialogue({ position, size, dialogueNumber, location, dis
                 />
             );
         }
-        if (dialogueNumber == 5) {
+        else if (dialogueNumber == 5) {
             return (
                 <Image
                     style={{
@@ -81,6 +87,106 @@ export default function Dialogue({ position, size, dialogueNumber, location, dis
                     source={gotKey}
                 />
             );
+        }
+        else if (dialogueNumber == 6) {
+            return (
+                <Image
+                    style={{
+                        width: size,
+                        height: size * 0.4,
+                        position: "absolute",
+                        left: position[0] * size,
+                        top: location * (size * 0.6),
+                    }}
+                    resizeMode="stretch"
+                    source={extraDialogue1}
+                />
+            );
+        }
+        else if (dialogueNumber == 7) {
+            return (
+                <Image
+                    style={{
+                        width: size,
+                        height: size * 0.4,
+                        position: "absolute",
+                        left: position[0] * size,
+                        top: location * (size * 0.6),
+                    }}
+                    resizeMode="stretch"
+                    source={extraDialogue2}
+                />
+            );
+        }
+        else if (dialogueNumber == 8) {
+            return (
+                <Image
+                    style={{
+                        width: size,
+                        height: size * 0.4,
+                        position: "absolute",
+                        left: position[0] * size,
+                        top: location * (size * 0.6),
+                    }}
+                    resizeMode="stretch"
+                    source={extraDialogue3}
+                />
+            );
+        }
+        else if (dialogueNumber == 9) {
+            return (
+                <Image
+                    style={{
+                        width: size,
+                        height: size * 0.4,
+                        position: "absolute",
+                        left: position[0] * size,
+                        top: location * (size * 0.6),
+                    }}
+                    resizeMode="stretch"
+                    source={unlockedDialogue1}
+                />
+            );
+        }
+        else if (dialogueNumber == 10) {
+            return (
+                <Image
+                    style={{
+                        width: size,
+                        height: size * 0.4,
+                        position: "absolute",
+                        left: position[0] * size,
+                        top: location * (size * 0.6),
+                    }}
+                    resizeMode="stretch"
+                    source={unlockedDialogue2}
+                />
+            );
+        }
+        else if (dialogueNumber == 11) {
+            return (
+                <Image
+                    style={{
+                        width: size,
+                        height: size * 0.4,
+                        position: "absolute",
+                        left: position[0] * size,
+                        top: location * (size * 0.6),
+                    }}
+                    resizeMode="stretch"
+                    source={unlockedDialogue3}
+                />
+            );
+        }
+        else {
+            return (
+                <Image style={{
+                    width: 0,
+                    height: 0,
+                }}
+                    source={message1}
+                />
+            )
         }
     }
     else {
