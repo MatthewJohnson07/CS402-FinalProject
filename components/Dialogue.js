8ecee755bef04b3e91bd109969931a5498ed53e7
rossrippee <74170962+rossrippee@users.noreply.github.com>
2022-05-03 13:10:38 -0600

    Dialogue now includes key acquired message

--- components/Dialogue.js
+++ components/Dialogue.js
@@ -4,6 +4,7 @@ const message1 = require('../assets/keyDialogue1.png');
 const message2 = require('../assets/keyDialogue2.png');
 const message3 = require('../assets/keyDialogue3.png');
 const message4 = require('../assets/keyDialogue4.png');
+const gotKey = require('../assets/keyAcquired.png');
 export default function Dialogue({ position, size, dialogueNumber, location, display }) {
     if (display) {
         if (dialogueNumber == 1) {
@@ -63,6 +64,21 @@ export default function Dialogue({ position, size, dialogueNumber, location, dis
                     }}
                     resizeMode="stretch"
                     source={message4}
+                />
+            );
+        }
+        if (dialogueNumber == 5) {
+            return (
+                <Image
+                    style={{
+                        width: size,
+                        height: size * 0.4,
+                        position: "absolute",
+                        left: position[0] * size,
+                        top: location * (size * 0.6),
+                    }}
+                    resizeMode="stretch"
+                    source={gotKey}
                 />
             );
         }
